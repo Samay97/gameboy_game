fd49db3260a3181ce63bf6ca43218685abe3eba0
Samuel Knoch <sknoch@mvision.lan>
2020-01-28 12:43:00 +0100

    test

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
-C:\Users\sknoch\Desktop\GB\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
-C:\Users\sknoch\Desktop\GB\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
+.\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
+.\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
