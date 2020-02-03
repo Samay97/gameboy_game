b4c4a98c98e44f84c83c6bda62da0a00111bddb5
Samuel Knoch <sknoch@mvision.lan>
2020-02-03 20:08:59 +0100

    Add alle setup files and basic config

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <gb/gb.h>
+#include "sprites/smiler.c"
+
+void main() {
+    while(1) {
+        switch(joypad()) {
+            case J_LEFT:
+                printf("Left!\n");
+                delay(100);
+                break;
+            case J_RIGHT:
+                printf("Right!\n");
+                delay(100);
+                break;
+            case J_UP:
+                printf("Up!\n");
+                delay(100);
+                break;
+            case J_DOWN:
+                printf("Down!\n");
+                delay(100);
+                break;
+            case J_START:
+                printf("Start!\n");
+                delay(100);
+                break;
+            case J_SELECT:
+                printf("Select!\n");
+                delay(100);
+                break;
+            case J_A:
+                printf("A!\n");
+                delay(100);
+                break;
+            case J_B:
+                printf("B!\n");
+                delay(100);
+                break;
+        }
+    }
+}
